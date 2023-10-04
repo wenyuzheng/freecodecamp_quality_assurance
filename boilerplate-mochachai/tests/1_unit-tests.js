11b12cb883f93e3c161c525c2de03fb9836ba386
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-04 10:53:02 +0100

    Complete equality compariso

--- boilerplate-mochachai/tests/1_unit-tests.js
+++ boilerplate-mochachai/tests/1_unit-tests.js
@@ -25,12 +25,12 @@ suite("Unit Tests", function () {
     });
     // #4
     test("#isTrue, #isNotTrue", function () {
-      assert.fail(true, "true is true");
-      assert.fail(
+      assert.isTrue(true, "true is true");
+      assert.isTrue(
         !!"double negation",
         "Double negation of a truthy value is true"
       );
-      assert.fail(
+      assert.isNotTrue(
         { value: "truthy" },
         "Objects are truthy, but are not boolean values"
       );
@@ -42,26 +42,30 @@ suite("Unit Tests", function () {
   suite("Equality", function () {
     // #5
     test("#equal, #notEqual", function () {
-      assert.fail(12, "12", "Numbers are coerced into strings with ==");
-      assert.fail({ value: 1 }, { value: 1 }, "== compares object references");
-      assert.fail(6 * "2", "12");
-      assert.fail(6 + "2", "12");
+      assert.equal(12, "12", "Numbers are coerced into strings with ==");
+      assert.notEqual(
+        { value: 1 },
+        { value: 1 },
+        "== compares object references"
+      );
+      assert.equal(6 * "2", "12");
+      assert.notEqual(6 + "2", "12");
     });
     // #6
     test("#strictEqual, #notStrictEqual", function () {
-      assert.fail(6, "6");
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * "2", 12);
-      assert.fail([1, "a", {}], [1, "a", {}]);
+      assert.notStrictEqual(6, "6");
+      assert.strictEqual(6, 3 * 2);
+      assert.strictEqual(6 * "2", 12);
+      assert.notStrictEqual([1, "a", {}], [1, "a", {}]);
     });
     // #7
     test("#deepEqual, #notDeepEqual", function () {
-      assert.fail(
+      assert.deepEqual(
         { a: "1", b: 5 },
         { b: 5, a: "1" },
         "The order of keys doesn't matter"
       );
-      assert.fail(
+      assert.notDeepEqual(
         { a: [5, 6] },
         { a: [6, 5] },
         "The order of array elements does matter"
@@ -78,22 +82,22 @@ suite("Unit Tests", function () {
   suite("Comparisons", function () {
     // #8
     test("#isAbove, #isAtMost", function () {
-      assert.fail("hello".length, 5);
-      assert.fail(1, 0);
-      assert.fail(Math.PI, 3);
-      assert.fail(1 - Math.random(), 1);
+      assert.isAtMost("hello".length, 5);
+      assert.isAbove(1, 0);
+      assert.isAbove(Math.PI, 3);
+      assert.isAtMost(1 - Math.random(), 1);
     });
     // #9
     test("#isBelow, #isAtLeast", function () {
-      assert.fail("world".length, 5);
-      assert.fail(2 * Math.random(), 0);
-      assert.fail(5 % 2, 2);
-      assert.fail(2 / 3, 1);
+      assert.isAtLeast("world".length, 5);
+      assert.isAtLeast(2 * Math.random(), 0);
+      assert.isBelow(5 % 2, 2);
+      assert.isBelow(2 / 3, 1);
     });
     // #10
     test("#approximately", function () {
-      assert.fail(weirdNumbers(0.5), 1, 0);
-      assert.fail(weirdNumbers(0.2), 1, 0);
+      assert.approximately(weirdNumbers(0.5), 1, 0.5);
+      assert.approximately(weirdNumbers(0.2), 1, 0.8);
     });
   });
 
@@ -104,11 +108,11 @@ suite("Unit Tests", function () {
   suite("Arrays", function () {
     // #11
     test("#isArray, #isNotArray", function () {
-      assert.fail(
+      assert.isArray(
         "isThisAnArray?".split(""),
         "String.prototype.split() returns an array"
       );
-      assert.fail([1, 2, 3].indexOf(2), "indexOf returns a number");
+      assert.isNotArray([1, 2, 3].indexOf(2), "indexOf returns a number");
     });
     // #12
     test("Array #include, #notInclude", function () {
