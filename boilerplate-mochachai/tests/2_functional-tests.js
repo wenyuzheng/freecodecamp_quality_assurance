eee7a7f73216d6887a39857a8d25e6ac5945f013
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-06 10:19:07 +0100

    Pass tests but has errors

--- boilerplate-mochachai/tests/2_functional-tests.js
+++ boilerplate-mochachai/tests/2_functional-tests.js
@@ -73,8 +73,8 @@ Browser.site = "http://localhost:3000/";
 
 suite("Functional Tests with Zombie.js", function () {
   const browser = new Browser();
-  suiteSetup((done) => {
-    return browser.visit("/", done());
+  suiteSetup(function (done) {
+    return browser.visit("/", done);
   });
 
   this.timeout(5000);
@@ -88,9 +88,15 @@ suite("Functional Tests with Zombie.js", function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", "Colombo").then(() => {
+        browser.pressButton("submit", () => {
+          browser.assert.success();
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#surname", "Colombo");
+          browser.assert.elements("span#dates", 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
