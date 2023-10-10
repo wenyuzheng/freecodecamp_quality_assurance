4704bec014bd6e0dcd4534902b1ccba41db04032
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-10 17:21:26 +0100

    Pass challenge of registration

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -42,6 +42,7 @@ myDB(async (client) => {
       title: "Connected to Database",
       message: "Please log in",
       showLogin: true,
+      showRegistration: true,
     });
   });
 
@@ -62,6 +63,32 @@ myDB(async (client) => {
     req.logout();
     res.redirect("/");
   });
+
+  app.route("/register").post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) next(err);
+        else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: req.body.password,
+            },
+            (err, doc) => {
+              if (err) res.redirect("/");
+              else {
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => res.redirect("/profile")
+  );
 
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
