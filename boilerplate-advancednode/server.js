99a81699123b07b613ebccdb2639c2253b0204ab
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-10 17:00:09 +0100

    Edit profile page

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -55,7 +55,7 @@ myDB(async (client) => {
     );
 
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile");
+    res.render("profile", { username: req.user.username });
   });
 
   passport.serializeUser((user, done) => {
