a227ece79133f3a0200e5c0d9eca56a13a9d9b7c
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-10 18:21:38 +0100

    Pass challenge for encrypting password

--- boilerplate-advancednode/server.js
+++ boilerplate-advancednode/server.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const passport = require("passport");
 const { ObjectID } = require("mongodb");
 const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
 
 const app = express();
 
@@ -71,10 +72,11 @@ myDB(async (client) => {
         else if (user) {
           res.redirect("/");
         } else {
+          const hash = bcrypt.hashSync(req.body.password, 12);
           myDataBase.insertOne(
             {
               username: req.body.username,
-              password: req.body.password,
+              password: hash,
             },
             (err, doc) => {
               if (err) res.redirect("/");
@@ -110,7 +112,8 @@ myDB(async (client) => {
         console.log(`User ${username} attempted to log in`);
         if (err) return done(err);
         if (!user) return done(null, false);
-        if (password !== user.password) return done(null, false);
+        if (!bcrypt.compareSync(password, user.password))
+          return done(null, false);
         return done(null, user);
       });
     })
