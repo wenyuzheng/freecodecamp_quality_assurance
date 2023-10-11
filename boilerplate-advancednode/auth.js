afdd795369f9b0958a47efeb6b51329583d0ae63
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-11 16:29:54 +0100

    Complete social authentication challenges

--- boilerplate-advancednode/auth.js
+++ boilerplate-advancednode/auth.js
@@ -37,6 +37,30 @@ module.exports = (app, myDataBase) => {
       },
       (accessToken, refreshToken, profile, cb) => {
         console.log(profile);
+        myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              username: profile.username,
+              name: profile.displayName || "John Doe",
+              photo: profile.photos[0].value || "",
+              email: Array.isArray(profile.emails)
+                ? profile.emails[0].value
+                : "No public email",
+              created_on: new Date(),
+              provider: profile.provider || "",
+            },
+            $set: {
+              last_login: new Date(),
+            },
+            $inc: {
+              login_count: 1,
+            },
+          },
+          { upsert: true, new: true },
+          (err, doc) => cb(null, doc.value)
+        );
       }
     )
   );
