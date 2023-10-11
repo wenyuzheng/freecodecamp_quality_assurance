6edd1a098dc09766c6c078ab38694bffd65ed9b8
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-11 17:29:45 +0100

    pass set up the environment challenge

--- boilerplate-advancednode/public/client.js
+++ boilerplate-advancednode/public/client.js
@@ -1,9 +1,11 @@
+const socket = io();
+
 $(document).ready(function () {
   // Form submittion with new message in field with id 'm'
-  $('form').submit(function () {
-    var messageToSend = $('#m').val();
+  $("form").submit(function () {
+    var messageToSend = $("#m").val();
 
-    $('#m').val('');
+    $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
 });
