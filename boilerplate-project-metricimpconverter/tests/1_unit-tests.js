fc22cefa07ddb2c08e63752ebae4cfd95cb941fe
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-14 14:49:38 +0100

    wrote tests for getNum()

--- boilerplate-project-metricimpconverter/tests/1_unit-tests.js
+++ boilerplate-project-metricimpconverter/tests/1_unit-tests.js
@@ -1,9 +1,45 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+suite("Unit Tests", function () {
+  suite("Read number input", () => {
+    test("Should read a whole number input", () => {
+      const result = convertHandler.getNum("2km");
+      const expected = 2;
+      assert.strictEqual(result, expected);
+    });
 
+    test("Should read a decimal number input", () => {
+      const result = convertHandler.getNum("2.1km");
+      const expected = 2.1;
+      assert.strictEqual(result, expected);
+    });
+
+    test("Should read a fractional number input", () => {
+      const result = convertHandler.getNum("3/2km");
+      const expected = 1.5;
+      assert.strictEqual(result, expected);
+    });
+
+    test("Should read a fractional number input with a decimal", () => {
+      const result = convertHandler.getNum("3.2/5km");
+      const expected = 0.64;
+      assert.strictEqual(result, expected);
+    });
+
+    test("Should return an error on a double-fraction", () => {
+      const result = convertHandler.getNum("3/2/3km");
+      const expected = "Invalid number";
+      assert.strictEqual(result, expected);
+    });
+
+    test("Should return 1 when no numerical input", () => {
+      const result = convertHandler.getNum("km");
+      const expected = 1;
+      assert.strictEqual(result, expected);
+    });
+  });
 });
