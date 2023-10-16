582989007a5c16e24c47d553e45e37742adf7c75
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-16 10:08:31 +0100

    Add functional tests

--- boilerplate-project-metricimpconverter/routes/api.js
+++ boilerplate-project-metricimpconverter/routes/api.js
@@ -1,10 +1,20 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
+
+const express = require("express");
+
+const app = express();
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
+  app.get("/api/convert", (req, res) => {
+    const { input } = req.query;
 
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+
+    res.json({ initNum, initUnit });
+  });
 };
--- boilerplate-project-metricimpconverter/tests/2_functional-tests.js
+++ boilerplate-project-metricimpconverter/tests/2_functional-tests.js
@@ -1,10 +1,80 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 let assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  test("Test GET /api/conver?input=10L", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/convert?input=10L")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          initNum: 10,
+          initUnit: "L",
+          returnNum: 2.64172,
+          returnUnit: "gal",
+          string: "10 liters converts to 2.64172 gallons",
+        });
+        done();
+      });
+  });
 
+  test("Test GET /api/conver?input=32g", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/convert?input=32g")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, "invalid unit");
+        done();
+      });
+  });
+
+  test("Test GET /api/conver?input=3/7.2/4kg", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/convert?input=3/7.2/4kg")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, "invalid number");
+        done();
+      });
+  });
+
+  test("Test GET /api/conver?input=3/7.2/4kilomegagram", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/convert?input=3/7.2/4kilomegagram")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, "invalid number");
+        done();
+      });
+  });
+
+  test("Test GET /api/conver?input=kg", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/convert?input=kg")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {
+          initNum: 1,
+          initUnit: "kg",
+          returnNum: 2.20462,
+          returnUnit: "lbs",
+          string: "1 kilograms converts to 2.20462 pounds",
+        });
+        done();
+      });
+  });
 });
