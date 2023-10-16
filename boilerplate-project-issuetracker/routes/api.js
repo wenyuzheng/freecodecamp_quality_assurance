90fe3fd4f3cc594cee420a3b1259ea8cc5195944
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-16 11:48:24 +0100

    In progress post action

--- boilerplate-project-issuetracker/routes/api.js
+++ boilerplate-project-issuetracker/routes/api.js
@@ -1,27 +1,33 @@
-'use strict';
+"use strict";
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      const { issue_title, issue_text, created_by, assigned_to, status_text } =
+        req.body;
+
+      return res.json({
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+        open: true,
+      });
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
