800932ae564783bc8b057bc0b2ce1745ba221b2c
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-16 12:17:07 +0100

    Connect mongoose and pass post action test cases

--- boilerplate-project-issuetracker/routes/api.js
+++ boilerplate-project-issuetracker/routes/api.js
@@ -1,6 +1,26 @@
 "use strict";
+const mongoose = require("mongoose");
 
 module.exports = function (app) {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const issueSchema = new mongoose.Schema({
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    status_text: String,
+    open: { type: Boolean, default: true },
+    created_on: { type: Date, required: true },
+    updated_on: { type: Date, required: true },
+    project: { type: String, required: true },
+  });
+
+  const Issue = mongoose.model("Issue", issueSchema);
+
   app
     .route("/api/issues/:project")
 
@@ -9,17 +29,28 @@ module.exports = function (app) {
     })
 
     .post(function (req, res) {
-      let project = req.params.project;
+      const project = req.params.project;
       const { issue_title, issue_text, created_by, assigned_to, status_text } =
         req.body;
 
-      return res.json({
+      if (!issue_title || !issue_text || !created_by)
+        return res.json({ error: "required field(s) missing" });
+
+      const newIssue = new Issue({
         issue_title,
         issue_text,
         created_by,
-        assigned_to,
-        status_text,
+        assigned_to: assigned_to ?? "",
+        status_text: status_text ?? "",
         open: true,
+        created_on: new Date(),
+        updated_on: new Date(),
+        project,
+      });
+
+      newIssue.save().then((savedIssue, err) => {
+        if (err) return res.json({ error: err });
+        return res.json(savedIssue);
       });
     })
 
--- boilerplate-project-issuetracker/tests/2_functional-tests.js
+++ boilerplate-project-issuetracker/tests/2_functional-tests.js
@@ -31,41 +31,41 @@ suite("Functional Tests", function () {
         });
     });
 
-    // test("POST /api/issues/apitest with only required fields", function (done) {
-    //   chai
-    //     .request(server)
-    //     .keepOpen()
-    //     .post("/api/issues/apitest")
-    //     .send({
-    //       issue_title: "test1",
-    //       issue_text: "test1",
-    //       created_by: "test1",
-    //     })
-    //     .end(function (err, res) {
-    //       assert.equal(res.status, 200);
-    //       assert.strictEqual(res.body.issue_title, "test1");
-    //       assert.strictEqual(res.body.issue_text, "test1");
-    //       assert.strictEqual(res.body.created_by, "test1");
-    //       assert.strictEqual(res.body.assigned_to, "");
-    //       assert.strictEqual(res.body.status_text, "");
-    //       assert.isTrue(res.body.open);
-    //       done();
-    //     });
-    // });
+    test("POST /api/issues/apitest with only required fields", function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .post("/api/issues/apitest")
+        .send({
+          issue_title: "test1",
+          issue_text: "test1",
+          created_by: "test1",
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.strictEqual(res.body.issue_title, "test1");
+          assert.strictEqual(res.body.issue_text, "test1");
+          assert.strictEqual(res.body.created_by, "test1");
+          assert.strictEqual(res.body.assigned_to, "");
+          assert.strictEqual(res.body.status_text, "");
+          assert.isTrue(res.body.open);
+          done();
+        });
+    });
 
-    // test("POST /api/issues/apitest with missing required fields", function (done) {
-    //   chai
-    //     .request(server)
-    //     .keepOpen()
-    //     .post("/api/issues/apitest")
-    //     .send({
-    //       issue_title: "test1",
-    //     })
-    //     .end(function (err, res) {
-    //       assert.equal(res.status, 200);
-    //       assert.deepEqual(res.body, { error: "required field(s) missing" });
-    //       done();
-    //     });
-    // });
+    test("POST /api/issues/apitest with missing required fields", function (done) {
+      chai
+        .request(server)
+        .keepOpen()
+        .post("/api/issues/apitest")
+        .send({
+          issue_title: "test1",
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.deepEqual(res.body, { error: "required field(s) missing" });
+          done();
+        });
+    });
   });
 });
