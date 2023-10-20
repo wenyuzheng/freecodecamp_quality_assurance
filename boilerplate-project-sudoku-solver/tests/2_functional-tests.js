886836be3d3a557eec145019383460f506176964
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-20 17:22:18 +0100

    Add tests for /api/check

--- boilerplate-project-sudoku-solver/tests/2_functional-tests.js
+++ boilerplate-project-sudoku-solver/tests/2_functional-tests.js
@@ -79,4 +79,182 @@ suite("Functional Tests", () => {
         });
     });
   });
+
+  suite("/api/check", () => {
+    test("valid placement", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+          coordinate: "A2",
+          value: 3,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { valid: true });
+          done();
+        });
+    });
+
+    test("invalid placement - conflict in row", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+          coordinate: "A2",
+          value: 4,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { valid: false, conflict: ["row"] });
+          done();
+        });
+    });
+
+    test("invalid placement - conflict in column", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+          coordinate: "A2",
+          value: 9,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { valid: false, conflict: ["column"] });
+          done();
+        });
+    });
+
+    test("invalid placement - conflict in row, column, region", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+          coordinate: "A4",
+          value: 1,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, {
+            valid: false,
+            conflict: ["row", "column", "region"],
+          });
+          done();
+        });
+    });
+
+    test("valid placement on occupied place", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+          coordinate: "A1",
+          value: 1,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { valid: true });
+          done();
+        });
+    });
+
+    test("return an error with missing puzzle field", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({})
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: "Required field missing" });
+          done();
+        });
+    });
+
+    test("return an error with invalid characters", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "..5....84..63.12...2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37-",
+          coordinate: "A1",
+          value: 1,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: "Invalid characters in puzzle" });
+          done();
+        });
+    });
+
+    test("return an error with incorrect length", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37",
+          coordinate: "A1",
+          value: 1,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, {
+            error: "Expected puzzle to be 81 characters long",
+          });
+          done();
+        });
+    });
+
+    test("return an error with incorrect input coordinate eg1", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37",
+          coordinate: "A0",
+          value: 0,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: "Invalid coordinate" });
+          done();
+        });
+    });
+
+    test("return an error with incorrect input coordinate eg2", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37",
+          coordinate: "A",
+          value: 0,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: "Invalid coordinate" });
+          done();
+        });
+    });
+
+    test("return an error with incorrect input number", (done) => {
+      chai
+        .request(server)
+        .post("/api/check")
+        .send({
+          puzzle:
+            "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37",
+          coordinate: "A1",
+          value: 0,
+        })
+        .end((err, res) => {
+          assert.deepEqual(res.body, { error: "Invalid value" });
+          done();
+        });
+    });
+  });
 });
