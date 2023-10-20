31774a37cdc35f7a955e5827cbc94d6a253df802
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-20 17:25:44 +0100

    Pass test for missing required field

--- boilerplate-project-sudoku-solver/routes/api.js
+++ boilerplate-project-sudoku-solver/routes/api.js
@@ -5,7 +5,11 @@ const SudokuSolver = require("../controllers/sudoku-solver.js");
 module.exports = function (app) {
   const solver = new SudokuSolver();
 
-  app.route("/api/check").post((req, res) => {});
+  app.route("/api/check").post((req, res) => {
+    const { puzzle, coordinate, value } = req.body;
+    if (!puzzle || !coordinate || !value)
+      return res.json({ error: "Required field missing" });
+  });
 
   app.route("/api/solve").post((req, res) => {
     const { puzzle } = req.body;
