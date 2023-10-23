1a8c5c8a79c5007d389b78bff26815cd5076c11e
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-23 11:16:51 +0100

    Pass four tests for toBritish

--- boilerplate-project-american-british-english-translator/components/translator.js
+++ boilerplate-project-american-british-english-translator/components/translator.js
@@ -1,10 +1,44 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  toBritish(text) {
+    let punctuation = "";
+    const lastChar = text[text.length - 1];
+    if (!/[a-zA-Z]/.test(lastChar)) {
+      punctuation = lastChar;
+      text = text.slice(0, -1);
+    }
+    const words = text.split(" ");
+    console.log({ punctuation, words });
 
+    const convertedWords = words.map((w) => {
+      console.log({ w });
+
+      if (americanOnly[w]) {
+        console.log(americanOnly[w]);
+        return americanOnly[w];
+      }
+
+      if (americanToBritishSpelling[w]) {
+        console.log(americanToBritishSpelling[w]);
+        return americanToBritishSpelling[w];
+      }
+
+      if (americanToBritishTitles[w]) {
+        console.log(americanToBritishTitles[w]);
+        return americanToBritishTitles[w];
+      }
+      return w;
+    });
+
+    const translated = convertedWords.join(" ") + punctuation;
+    return translated;
+  }
+
+  toAmerican(text) {}
 }
 
 module.exports = Translator;
